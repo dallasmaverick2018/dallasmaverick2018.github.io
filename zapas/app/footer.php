755325a0d22dd711cb9b870211b8ea7c9615e3aa
dinarey007 <rdinar007@mail.ru>
2017-02-02 23:49:34 +0500

    first commit1

--- zapas/app/footer.php
+++
@@ -1,8 +0,0 @@
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="public/js/bootstrap.js"></script>
-    <script src="public/js/carousel.js"></script>
-
-</body>
-</html>
